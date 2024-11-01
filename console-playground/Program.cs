911258e39e2c1250c2d40ae3d5ad0c37821187c5
Ifeanyi Oraelosi <ifeanyioraelosi@gmail.com>
2024-11-01 22:01:48 +0100

    test NLua package

--- console-playground/Program.cs
+++ console-playground/Program.cs
@@ -1,5 +1,6 @@
 ﻿using BenchmarkDotNet.Running;
 using Benchmarks;
+using NLua;
 
 namespace Main
 {
@@ -8,6 +9,21 @@ namespace Main
         public static void Main(string[] args)
         {
             var benchmarkSummary = BenchmarkRunner.Run<LinqQueryBenchmark>();
+            var state = new Lua();
+            state.DoString("""
+                           
+                           		function ScriptFunc (val1, val2)
+                           			if val1 > val2 then
+                           				return val1 + 1
+                           			else
+                           				return val2 - 1
+                           			end
+                           		end
+                           		
+                           """);
+            var scriptFunc = state["ScriptFunc"] as LuaFunction;
+            var res = (int)(long)scriptFunc.Call(3, 5).First();
+            Console.WriteLine(res);
         }
     }
 }
